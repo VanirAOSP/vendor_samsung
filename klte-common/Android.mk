f32530a29ff38a14474ddaa893b88b9f09e8b89b
DHOMD <DHO@dho.im>
2015-11-17 18:07:41 -0500

    Full reconcile with Muppets for Klte
    Tere has to be taint here, can readd anything important
    
    Change-Id: I8e3710a081d54bb56152109502f074157ebad010

--- klte-common/Android.mk
+++ klte-common/Android.mk
@@ -16,12 +16,12 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter klte kltedv kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter klte kltechn kltechnduo klteduos kltedv kltekdi kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/app/TimeService.apk
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
